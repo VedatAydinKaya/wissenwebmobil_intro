26e99a46442b3c1bac0e52d44b750f1a2fa863b9
Vedat Kaya <vedataydinkayaa@gmail.com>
2021-10-28 11:42:45 +0300

    zar oyunu added

--- ZarOyunu/Program.cs
+++ ZarOyunu/Program.cs
@@ -10,45 +10,55 @@ namespace ZarOyunu
               Kullanıcıdan 1-6 arası bir zar bilgisi alınacak. Örnek olarak kullanıcı 6 seçti. program 6 için 
  2 zarı 6-6 gelene kadar rastgele atacak. kaçıncı denemede çifti bulduğunu ekranda yazan programı yazınız.*/
 
-            int tahmin = 0, girilenSayi = 0;
-            Random rnd = new Random();
             do
             {
-                Console.Write("ZAR OYUNU ICIN SANSLI SAYINIZI GIRINIZ: ");
-
-                try
+                int tahmin = 0, girilenSayi = 0;
+                Random rnd = new Random();
+                do
                 {
+                    Console.Write("ZAR OYUNU ICIN SANSLI SAYINIZI GIRINIZ: ");
 
-                    girilenSayi = int.Parse(Console.ReadLine());
-                    if (girilenSayi < 1 || girilenSayi > 6)
-                        throw new Exception("1-6 arasında bir sayi girmediniz:");
-                    // break;
-                }
-                catch (Exception ex)
-                {
+                    try
+                    {
 
-                    Console.WriteLine("Lutfen sayi giriniz:");
-                    Console.WriteLine(ex.Message);
-                    continue;
+                        girilenSayi = int.Parse(Console.ReadLine());
+                        if (girilenSayi < 1 || girilenSayi > 6)
+                            throw new Exception("1-6 arasında bir sayi girmediniz:");
+                        // break;
+                    }
+                    catch (Exception ex)
+                    {
 
-                }
+                        Console.WriteLine("Lutfen sayi giriniz:");
+                        Console.WriteLine(ex.Message);
+                        continue;
 
-                break;
+                    }
 
-            } while (true);
-            do
-            {
-              
-                int randomsayi1 = rnd.Next(1, 7);
-                int randomsayi2 = rnd.Next(1, 7);
-                tahmin++;
-                if (randomsayi1 == girilenSayi && randomsayi2 == girilenSayi)
-                {
-                    Console.WriteLine($"Tebrikler {tahmin}.denemede ugurlu sayiniz {girilenSayi} bulundu ");
                     break;
-                }
-                
-                //break;
+
+                } while (true);
+                do
+                {
+
+                    int randomsayi1 = rnd.Next(1, 7);
+                    int randomsayi2 = rnd.Next(1, 7);
+                    tahmin++;
+                    if (randomsayi1 == girilenSayi && randomsayi2 == girilenSayi)
+                    {
+                        Console.WriteLine($"Tebrikler {tahmin}.denemede ugurlu sayiniz {girilenSayi} bulundu ");
+                        break;
+                    }
+
+                    //break;
+                } while (true);
+
+                Console.WriteLine("TEKRAR OYNAMAK ICIN E YA BAS: ");
+                ConsoleKeyInfo info = Console.ReadKey();
+                if (info.Key != ConsoleKey.E)
+                    break;
+
+            
             } while (true);
 
 

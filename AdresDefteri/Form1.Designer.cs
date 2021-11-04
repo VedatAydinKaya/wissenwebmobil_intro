e5e7ce3d529d85746fa3c898260a7340096bbeee
Vedat Kaya <vedataydinkayaa@gmail.com>
2021-11-05 01:00:47 +0300

    ad-soyad checked

---
+++ AdresDefteri/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace AdresDefteri
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAd = new System.Windows.Forms.Label();
+            this.lblSoyad = new System.Windows.Forms.Label();
+            this.lblTCKN = new System.Windows.Forms.Label();
+            this.lblDogumTarihi = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtTCKN = new System.Windows.Forms.TextBox();
+            this.dtpDogumTarihi = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.BackColor = System.Drawing.SystemColors.Control;
+            this.lblAd.Location = new System.Drawing.Point(7, 18);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(22, 15);
+            this.lblAd.TabIndex = 0;
+            this.lblAd.Text = "Ad";
+            // 
+            // lblSoyad
+            // 
+            this.lblSoyad.AutoSize = true;
+            this.lblSoyad.Location = new System.Drawing.Point(4, 51);
+            this.lblSoyad.Name = "lblSoyad";
+            this.lblSoyad.Size = new System.Drawing.Size(39, 15);
+            this.lblSoyad.TabIndex = 1;
+            this.lblSoyad.Text = "Soyad";
+            // 
+            // lblTCKN
+            // 
+            this.lblTCKN.AutoSize = true;
+            this.lblTCKN.Location = new System.Drawing.Point(7, 84);
+            this.lblTCKN.Name = "lblTCKN";
+            this.lblTCKN.Size = new System.Drawing.Size(36, 15);
+            this.lblTCKN.TabIndex = 2;
+            this.lblTCKN.Text = "TCKN";
+            // 
+            // lblDogumTarihi
+            // 
+            this.lblDogumTarihi.AutoSize = true;
+            this.lblDogumTarihi.Location = new System.Drawing.Point(-1, 117);
+            this.lblDogumTarihi.Name = "lblDogumTarihi";
+            this.lblDogumTarihi.Size = new System.Drawing.Size(78, 15);
+            this.lblDogumTarihi.TabIndex = 3;
+            this.lblDogumTarihi.Text = "Dogum Tarihi";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(102, 18);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(200, 23);
+            this.txtAd.TabIndex = 4;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(102, 51);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(200, 23);
+            this.txtSoyad.TabIndex = 5;
+            // 
+            // txtTCKN
+            // 
+            this.txtTCKN.Location = new System.Drawing.Point(102, 81);
+            this.txtTCKN.Name = "txtTCKN";
+            this.txtTCKN.Size = new System.Drawing.Size(200, 23);
+            this.txtTCKN.TabIndex = 6;
+            // 
+            // dtpDogumTarihi
+            // 
+            this.dtpDogumTarihi.Location = new System.Drawing.Point(102, 117);
+            this.dtpDogumTarihi.Name = "dtpDogumTarihi";
+            this.dtpDogumTarihi.Size = new System.Drawing.Size(200, 23);
+            this.dtpDogumTarihi.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtpDogumTarihi);
+            this.Controls.Add(this.txtTCKN);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.lblDogumTarihi);
+            this.Controls.Add(this.lblTCKN);
+            this.Controls.Add(this.lblSoyad);
+            this.Controls.Add(this.lblAd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.Label lblSoyad;
+        private System.Windows.Forms.Label lblTCKN;
+        private System.Windows.Forms.Label lblDogumTarihi;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtTCKN;
+        private System.Windows.Forms.DateTimePicker dtpDogumTarihi;
+    }
+}
+
---
+++ AdresDefteri/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdresDefteri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Kisi kisi = new Kisi();  // instance alındı
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                kisi.Ad = "Kamil";
+                kisi.Soyad = "hayriye";
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"Bir hata olustu: {ex.Message} ");
+            }
+
+          //  MessageBox.Show($" {kisi.Ad} {kisi.Soyad} ");
+
+
+
+        }
+
+      
+    }
+}
